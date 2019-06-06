fd54cc15485d7010562af3265c3910a0c240c808
Kristofer Selbekk <selbeezy@gmail.com>
2019-06-06 08:58:18 +0200

    Add more initial data

--- src/utils/initialState.js
+++ src/utils/initialState.js
@@ -1,20 +1,66 @@
 export const initialState = () => [
   {
-    id: "1",
+    id: '1',
     createdDate: 1559670653398,
-    description: "Et vanlig bilde",
-    url: "https://www.bekk.no/",
+    description: 'En rolig morgen med kaffe, crossaint og charts',
+    url:
+      'https://uc7e7b4f8c58bd68a53e7e8e0459.previews.dropboxusercontent.com/p/thumb/AAc_dS1EV-eM-RGV9OCJy-gk9FqqgGmOeScyRkq0tdnvRBJ7RWBLDIUrf-bGhelo6RIifcIi9ED21UJ7S51h7Kv8_5w8SFQgmEUGBy7TCWiOK5NLLEsG9-sozf7va5kAr7pfJDU0PNCg-IrZGfS7P4RP3hjK6l-zRQKQseHtExKuOMfXLae7bhkbcoS5OWgKF83d_HwgXFhH5CkXDYtmgoaVZElbYRG3tgzykotV014FPQd72fxPJdVCQplvyAkHlBT2NH4E0BIL2bHg-Ko7_ewFhopvsQyiPkAu6wzv5ZEkwIXdh8kGsF0LjHwjtszwBNXf2AIh1POds8NJibdnskFcEmZTEgOlT8PksEMNGHezyZgfuRNeskLDUuA9lR-C8_evfLZ9Ix-XE-7HNdeMRKHQTPU3shYA2Q9zYkrLmLFXB4bk5xdpp11fJeZvNBVYiRbbyKCpunaGco6ESNBV21TWoVZdre6n8Lx_gj7-qweZFOXEvmW1wI4J6lNhzqNxJsXROExYnJsRBtjxIvtRcqhGrAolBvz5SZZUHXGB-9TjK5-nwyveNNbenTICSq4yYls/p.jpeg?fv_content=true&size_mode=5',
+    username: 'selbekk',
     comments: [
       {
-        id: "2",
+        id: '2',
         createdDate: 1559670668735,
-        text: "Dette er en kommentar"
-      }
+        text: 'Utrolig stilig',
+        username: 'markusra',
+      },
     ],
     likes: [
       {
-        username: "ola.nordmann"
-      }
-    ]
-  }
+        username: 'marie.buoen',
+      },
+      {
+        username: 'markusra',
+      },
+    ],
+  },
+  {
+    id: '2',
+    createdDate: 1559670853398,
+    description: 'Det jobbes! #awesome',
+    url:
+      'https://uc162c4227efc085885070b1ef54.previews.dropboxusercontent.com/p/thumb/AAfn6I2Pke0rwZPcpDFZvElvfFOe6Zu35eEteXPprYD1_6fh_miuOEQxOnOj5FHO1oudP76RrFSsx2fh3A-FGhVXK3EkpSJCXiTUjMLssptRrO9oLHUBrH2Lkb9ZbyVyzfOK_CIQWuiR-Asxc-iR68unEcyFr32f-QL0IibVwN5c40qygCHuwDBzaFfDDrKgdwMMBOEUduxF2ZUaymsykh-yJ4Mmvl3ZOPuWOUf4F3ymPTUU4jtDq1J3SmpEchgo89jT9Qa9Ofcw51S6Hao6ZuNQSEyBI0XnR8OiMm0b3uslbzpmtMDmcHpr6Y3qZ5xi4WGISxbwtt38VQFxVBFBEVcJysyGq8DH1J23zVZaJIf6CvyP-tPqJCsYIvd5agtJkhdT1kQB7gE_w8oiEsoUAEY2OQ-URa9E-BzLVMTw7NUkl7UA8OmOXA6mF3zU2eKo3n1AaYZwQRyyia1-ZzsbCjecWYAwq8auWNYxgfrhBNmKrhKCbImnWAqOFORBXHhHWQ_oecneWfxcSOhumG_p2VDTU-hu4aqGQN_eNlJNJn57blb00FfYZIZIVVQFbV4CDl4/p.jpeg?fv_content=true&size_mode=5',
+    username: 'selbekk',
+    comments: [],
+    likes: [],
+  },
+  {
+    id: '3',
+    createdDate: 1559671853398,
+    description: 'Samarbeid på arbeidsplassen',
+    url:
+      'https://ucb6a744cdbc86e4dcc58684216e.previews.dropboxusercontent.com/p/thumb/AAcZO-21LZFzemubycvyW_-R7yB1PJRnuFs_HJ7C2ajcGAL2t9g-l7RrudMHZVl3CAkK3AaxAcUBRLBYvjHUZr2B9nWHaZ-zyMBmLECY18UP9_q5_kZ_cv2wBx7ErHBBVWwLAvHmkBcR9-YL6b-PNby8wZawHTpfFmc-Otf6--Vfx7I_-NYLwAqjsT8bXoTrd4GrKbpksM3T8SlnwO0wFvY5RqChw6RAc1gOe3mG12bpSsumvkrqHbbbeJ0fgGIQVi7M7aqyT2Tg42ZuFRGAjKOpvcOLKN2ZKmOceIWx19zmkhjLZ2LhZXHXhNTSbrI6snEEyd2u60tOTkf-Kdsw2wU6c6abqClw4wS4Pav3r5AgvXyJIoCpYxZ0VKVr5okfyITYxpq9SyNbkSmj2Or92EuTBApu9QsGULI7gRlfuUAIdDFJ17lEjfbG0fyYe376ybqyouVIAXfPXSY35PmsJZYSUpRSQlTJTm8s1fujcbF1weJH8_QmjVUnniKO6MrGzYeymOABQHz7Dtq3j6BLhrjdLk5DnP-ZlEQ3ON1erJpopKfK7dILSM9A8GrJV63e8Sg/p.jpeg?fv_content=true&size_mode=5',
+    username: 'markusra',
+    comments: [],
+    likes: [],
+  },
+  {
+    id: '4',
+    createdDate: 1559671853398,
+    description: 'Full kreativ frihet',
+    url:
+      'https://uc4babb3573e639bfaa1203cbde9.previews.dropboxusercontent.com/p/thumb/AAcy3NeLDM_KP3FyaOQjmWJmu3wLFv5xAva-3Gh8lY6qLzrTiv9RzuflISxcetDmNpdD-DpaptCa4kv7TW7JdX5XXhGABtE_Em0Uixa7XaeEhANnc7dQk_Lerf3QMXeNYm__ALNe26NJQyNE_p7h41P1y9CKlzUeKXcKjjigVKzXDkWe89A9XehJGJ1-UjGMgC0yYNXGYmoX8qhOEA8NrKmQiXH2nVqUm0Rq7j0IVSKikfNs9uEsCFVIJRTNpONKwMmwz3kEDXnaVUykJNzrU9lvNg4geHBXmKDzLofjInBF3mQXJkwnkTn7fh1TjzIX1q_v8zu_yWA0W9QJ8PwNpOkFl3RhPffG-6KB9SvKS51_4mDOKUbWgkw3uHf3lMQG9OvhVusRauVDqYZf8GDAbmdonZdW6mU8ND--H7oCKeaEqkO500TSCGi-NzPxeNha-fmlRsxDA2b4IhUw-ufEK5ubqTLBbrc88_w8_XXppApVaOpuihu7D8fHGbz-Lf60p0qi55NbugYnrJNy7yi7xNCUH2vB44EitU9N7brjLooaNeRvTtEyHrtzyrMqo56ayNM/p.jpeg?fv_content=true&size_mode=5',
+    username: 'livetibekk',
+    comments: [],
+    likes: [],
+  },
+  {
+    id: '5',
+    createdDate: 1559671883398,
+    description: 'Når man ser på pull requests sammen',
+    url:
+      'https://ucaaa64654c5df42065d7719b0c0.previews.dropboxusercontent.com/p/thumb/AAdVIKk3bI8Zu7lbqFjbZ9RFFFva6W_KyCV1b2LhvA6JAsappnxMmurC98JzHozDgnaZjYRB7Hsan5gXQSQSZcUwAewmBNf5Y_Pny4mDofaNU8jBsZ7boXOTXcD_1f-sYuVC5g0lCe7xANp2OaLaFstqhymtVQHm1GWdRqhCAA-iyjWaaP-Y4bzcM794EH6HuOBA_MN07Wg86k25ln4C2b2OEj-h1oakGRwsaTXgMasKxu1qiAJvzqjuOGgrwSQxBFZ8u8IaXU3kqEWRWkuiDCwWnQrN9z7U4WNxpRQhcT3N04uWZNdASYZLWqHNr48xc-LIXYgg_b9wbCHMbGVF_5U1ReS4sSBobSlUSDHaYhbHTw5714S89EAE35ZJc0NgmTF16A-5CE7oouZxkzUvrqfkAzYGNxh0U-X8-aKKCCzrNRm_Tk9aaS6GPHXOi2keLB2pxLLsR1Mgboja17O8OqwPVTGZLnAdbmNBx-sU7SmiDqBPj9vqcXAv6xGTYhmzlkSWkonPgbDKHbR6Rv8rop5631fH3qpEFbx-4rBMi4Jl_1MIECg6QbtD_efUZ3v09H8/p.jpeg?fv_content=true&size_mode=5',
+    username: 'selbekk',
+    comments: [],
+    likes: [],
+  },
 ];
