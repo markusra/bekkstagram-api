5ea253ed2cccedfc38d4b439f9e41a8eb05a3303
Markus Rauhut <markus.rauhut@bekk.no>
2019-11-05 12:46:59 +0100

    Use error code 400 instead of 422

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,7 +4,7 @@ export const validRequest = (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        res.status(422).json({ errors: errors.array() });
+        res.status(400).json({ errors: errors.array() });
         return false;
     }
     return true;
